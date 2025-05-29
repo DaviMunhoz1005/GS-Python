04613ab68945f3893e6c09dca424c98d6affbdfb
Davi Munhoz <davimunhoz1005@gmail.com>
2025-05-29 17:51:08 -0300

    feat(interface): create standard interface for user

--- modules/interface.py
+++ modules/interface.py
@@ -1,3 +1,12 @@
 # Nome: Davi Marques de Andrade Munhoz     RM: 566223
 # Nome: Gabriel Ciriaco de Oliveira Silva  RM: 565916
-# Nome: Mariana SOuza França               RM: 562353
+# Nome: Mariana Souza França               RM: 562353
+
+def show_menu():
+    print("\n=========== MONITORAMENTO DE ENCHENTES NOS RIOS ===========")
+    print("1. Iniciar monitoramento em tempo real")
+    print("2. Cadastrar novo morador")
+    print("3. Listar moradores cadastrados")
+    print("4. Exibir histórico de alertas")
+    print("0. Sair")
+    return input("Escolha uma opção: ")
