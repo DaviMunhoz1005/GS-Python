983358d95fe21252f4710f8e3a2d1256a5f6cf6f
Davi Munhoz <davimunhoz1005@gmail.com>
2025-05-29 18:06:27 -0300

    feat(utils): create utilitary methods to validate and generate

--- modules/util.py
+++
@@ -1,3 +0,0 @@
-# Nome: Davi Marques de Andrade Munhoz     RM: 566223
-# Nome: Gabriel Ciriaco de Oliveira Silva  RM: 565916
-# Nome: Mariana Souza França               RM: 562353
---
+++ modules/utils.py
@@ -0,0 +1,21 @@
+# Nome: Davi Marques de Andrade Munhoz     RM: 566223
+# Nome: Gabriel Ciriaco de Oliveira Silva  RM: 565916
+# Nome: Mariana Souza França               RM: 562353
+
+# Biblioteca para expressões regulares
+import re
+
+# Bilioteca para manipular data e hora
+from datetime import datetime
+
+# valida se o formato do email está correto
+def validate_email(email:str) -> bool:
+    return bool(re.match(r"^[\w\.-]+@[\w\.-]+\.\w{2,}$", email))
+
+# valida se o formato do telefone está correto tendo apenas 11 dígitos
+def validate_phone(phone:str) -> bool:
+    return bool(re.match(r"^\d{11}$", phone))
+
+# gera a data e hora do momento que foi chamado em formato dia/mês/ano hora:minuto:segundo
+def generate_date_time() -> str:
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
